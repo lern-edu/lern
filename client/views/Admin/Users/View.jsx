edd30d3369f3f6655a2fe9acdfbd9f0edbc92ecc
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-12-19 03:35:06 -0200

    crete get users #2

--- client/views/Admin/Users/View.jsx
+++ client/views/Admin/Users/View.jsx
@@ -1,6 +1,13 @@
 // Libs
 import React from 'react';
+import _ from 'lodash';
+import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import { LinearProgress } from 'material-ui/Progress';
 import { Layout, Snackbar } from 'meteor/duckdodgerbrasl:lern-layouts';
+
+import AdminUsersList from './List.jsx';
 
 class AdminUsers extends React.Component {
 
@@ -11,7 +18,7 @@ class AdminUsers extends React.Component {
     const { skip=0 } = props;
     this.state = {
       query: {},
-      options: { skip, limit: 20 },
+      options: { skip },
       collections: {
         users: {
           handler: true,
@@ -25,19 +32,44 @@ class AdminUsers extends React.Component {
     const { query, options } = this.state;
 
     Meteor.call('AdminGetUsers', query, options,  (err, docs) => {
-      console.error(err);
       if (err) snack({ message: 'Erro ao encontrar usuários' });
       this.setState({ collections: { users: { handler: false, docs } } });
     });
   }
 
-  render() {
+  // Handlers
 
-    console.log(this.state.collections);
+  handleUpdateQuery({ target: { value: query } }) {
+    this.setState({ query });
+  }
+
+  render() {
+    const { collections, query } = this.state;
 
     return (
       <div>
         <Layout.Bar title='Usuários' />
+
+        {
+          !_.every(collections, c => !c.handler)
+          ? <LinearProgress color='accent' />
+          : (
+            [
+              <AppBar position='static' color='default' key='toolbar'>
+                <Toolbar>
+                  <TextField
+                    label='Pesquisar'
+                    placeholder='Pesquisar'
+                    onChange={this.handleUpdateQuery.bind(this)}
+                    fullWidth
+                    margin='normal'
+                  />
+                </Toolbar>
+              </AppBar>,
+              <AdminUsersList key='list' users={collections.users.docs} query={query} />,
+            ]
+          )
+        }
       </div>
     );
   }
