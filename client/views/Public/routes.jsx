aa2abee701690512398b1fe19af7294ead2aef5e
André Dornas <andredornas299@gmail.com>
2017-12-20 15:07:44 -0200

    temporary games view

--- client/views/Public/routes.jsx
+++ client/views/Public/routes.jsx
@@ -4,6 +4,8 @@ import { Setup } from 'meteor/duckdodgerbrasl:lern-layouts';
 
 // Views
 import PublicLogin from './Login/View.jsx';
+import PublicGames from './Games/View.jsx';
+import PublicGamesSudoku from './Games/Sudoku/View.jsx';
 
 const setup = new Setup();
 
@@ -12,6 +14,26 @@ FlowRouter.route('/', {
   action(params, query) {
     setup.render({
       main: <PublicLogin />,
+    });
+  },
+});
+
+FlowRouter.route('/games', {
+  name: 'PublicGames',
+  action(params, query) {
+    setup.render({
+      bar: true,
+      main: <PublicGames />,
+    });
+  },
+});
+
+FlowRouter.route('/games/sudoku', {
+  name: 'PublicGamesSudoku',
+  action(params, query) {
+    setup.render({
+      bar: true,
+      main: <PublicGamesSudoku />,
     });
   },
 });
