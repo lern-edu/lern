f20baeff06d4373246691fe475d052f460cb8c16
Douglas Eleuterio <douglaseleuterio@gmail.com>
2017-12-28 13:15:03 -0200

    finish crud view #2

--- client/views/Admin/User/View.jsx
+++ client/views/Admin/User/View.jsx
@@ -4,18 +4,20 @@ import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { Layout } from 'meteor/duckdodgerbrasl:lern-layouts';
 import { User } from 'meteor/duckdodgerbrasl:lern-model';
+import { Regex } from 'meteor/duckdodgerbrasl:lern-check';
 import { withStyles } from 'material-ui/styles';
 import { LinearProgress } from 'material-ui/Progress';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl, FormHelperText } from 'material-ui/Form';
 
 // Styles
 const styles = theme => ({
   paper: {
-    padding: 16,
-    margin: 16,
+    padding: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 2,
   },
 });
 
@@ -31,7 +33,7 @@ class AdminUser extends React.Component {
       collections: {
         user: {
           handler: !_.isEmpty(userId),
-          doc: !userId ? new User({ emails: [{ address: '' }] }) : null,
+          doc: !userId ? new User({ emails: [{ address: '' }], services: { password: '' } }) : null,
         },
       },
     };
@@ -41,10 +43,25 @@ class AdminUser extends React.Component {
     const { userId } = this.props;
 
     if (userId)
-      Meteor.call('AdminGetUsers', { _id: userId }, { limit: 1 },  (err, docs) => {
+      Meteor.call('AdminUsersGet', { _id: userId }, { limit: 1 },  (err, docs) => {
         if (err) snack({ message: 'Erro ao encontrar usuário' });
         this.setState({ collections: { user: { handler: false, doc: _.head(docs) } } });
       });
+  }
+
+  // Handlers
+
+  handleSubmit() {
+    const { collections: { user: { doc } } } = this.state;
+
+    doc.validate({ fields: ['roles', 'emails', 'profile.firstName', 'profile.lastName'] }, (err) => {
+      if (err) snack({ message: err.reason });
+      else
+        Meteor.call('AdminUserSave', doc, (err, res) => {
+          if (err) snack({ message: 'Erro ao salvar usuário' });
+          else FlowRouter.go('AdminUsers');
+        });
+    });
   }
 
   render() {
@@ -76,6 +93,42 @@ class AdminUser extends React.Component {
                             </Grid>
                           )
                         }
+
+                        <Grid item xs={12}>
+                          <doc.templates.FirstName form={this} doc={doc} />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                          <doc.templates.LastName form={this} doc={doc} />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                          <doc.templates.Roles form={this} doc={doc} />
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                          <Grid
+                            container
+                            alignItems='flex-end'
+                            direction='row'
+                            justify='flex-end'
+                          >
+                            <Grid item>
+                              <Button href={FlowRouter.path('AdminUsers')} raised>
+                                Cancel
+                              </Button>
+                            </Grid>
+
+                            <Grid item>
+                              <Button onClick={this.handleSubmit.bind(this)} raised color='primary'>
+                                Save
+                              </Button>
+                            </Grid>
+
+                          </Grid>
+
+                        </Grid>
 
                     </Grid>
 
