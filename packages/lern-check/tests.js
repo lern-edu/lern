2e98498466bcb70ce2ac264214c9d0b02977f714
André Dornas <andredornas299@gmail.com>
2018-01-06 13:54:03 -0200

    regex test

--- packages/lern-check/tests.js
+++ packages/lern-check/tests.js
@@ -1,4 +1,5 @@
 import { User } from 'meteor/duckdodgerbrasl:lern-model';
+import { Accounts } from 'meteor/accounts-base';
 import Check from 'meteor/duckdodgerbrasl:lern-check';
 import chai, { assert, expect } from 'chai';
 
@@ -90,6 +91,93 @@ describe('Check', function () {
       it('Has student role? no, it hasn\'t!', function () {
         expect(() => Check.User(user._id).role('student')).to.throw();
       });
+    }
+  });
+
+  describe('Regex', function () {
+
+    it('Is a function', function () {
+      assert.isFunction(Check.Regex);
+    });
+
+    if (Meteor.isServer) {
+      let valid = {};
+      let invalid = {};
+
+      beforeEach(function () {
+
+        valid = {
+          id: '8MZekqrgCkhQqaNty',
+          url: 'https://www.lern.com.br',
+          cnpj: '00000000000191',
+          cpf: '12323434556',
+          mail: 'mail@mail.com.br',
+          decimal: '432.90',
+        };
+
+        invalid = {
+          id: '8MZekqrgCkhqaNty',
+          url: 'https:/ww.lern.com.br',
+          cnpj: '0000d000000191',
+          cpf: '12323y34556',
+          mail: 'mailmail.com.br',
+          decimal: '432,',
+        };
+
+      });
+
+      describe('Valid obj', function () {
+        it('Check for validity id', function () {
+          expect(() => Check.Regex(valid.id).id()).to.not.throw();
+        });
+
+        it('Check for validity url', function () {
+          expect(() => Check.Regex(valid.url).url()).to.not.throw();
+        });
+
+        it('Check for validity cnpj', function () {
+          expect(() => Check.Regex(valid.cnpj).cnpj()).to.not.throw();
+        });
+
+        it('Check for validity cpf', function () {
+          expect(() => Check.Regex(valid.cpf).cpf()).to.not.throw();
+        });
+
+        it('Check for validity mail', function () {
+          expect(() => Check.Regex(valid.mail).mail()).to.not.throw();
+        });
+
+        it('Check for validity decimal', function () {
+          expect(() => Check.Regex(valid.decimal).decimal()).to.not.throw();
+        });
+      });
+
+      describe('Invalid obj', function () {
+        it('Check for invalidity id', function () {
+          expect(() => Check.Regex(invalid.id).id()).to.throw();
+        });
+
+        it('Check for invalidity url', function () {
+          expect(() => Check.Regex(invalid.url).url()).to.throw();
+        });
+
+        it('Check for invalidity cnpj', function () {
+          expect(() => Check.Regex(invalid.cnpj).cnpj()).to.throw();
+        });
+
+        it('Check for invalidity cpf', function () {
+          expect(() => Check.Regex(invalid.cpf).cpf()).to.throw();
+        });
+
+        it('Check for invalidity mail', function () {
+          expect(() => Check.Regex(invalid.mail).mail()).to.throw();
+        });
+
+        it('Check for invalidity decimal', function () {
+          expect(() => Check.Regex(invalid.decimal).decimal()).to.throw();
+        });
+      });
+
     }
   });
 
