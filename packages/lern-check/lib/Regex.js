dfe186e4306bd5735d7156aee4981b9e82ff8762
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-01-07 13:00:06 -0200

    wrong merge

--- packages/lern-check/lib/Regex.js
+++ packages/lern-check/lib/Regex.js
@@ -1,3 +1,11 @@
+import _ from 'lodash';
+
+/**
+ * Check user roles (Check.Regex()).
+ * @namespace Regex()
+ * @memberof LernCheck
+ */
+
 const patterns = {
   id: /^[a-zA-Z0-9]{17}$/i,
   url: /^(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/i,
@@ -5,53 +13,22 @@ const patterns = {
   cpf: /^[0-9]{11}$/,
   mail:  /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
   decimal: /^\d+\.?\d{0,}$/i,
-  password: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
 };
 
 /**
-* Check Regex expression (Check.Regex()).
-* @namespace Regex()
-* @memberof LernCheck
-*/
-
-/**
- * @desc Self description
  * @memberof LernCheck.Regex()
+ * @desc Self description
  * @example
- * const checkRegex = Check.Regex('id')
+ * const checkId = Check.Regex('8MZekqrgCkhQqaNty').id()
+ * const checkMultiplesId = Check.Regex('8MZekqrgCkhQqaNty', '9MAekhrgTkhaqa3ry').id()
+ * const checkUrl = Check.Regex('www.lern.com.br').url()
  * @public
- * @param {String} type - pre defined pattern
+ * @param {...String} args - Values to be checked
+ * @throws {Meteor.Error} regex-police-alarm - if param not match regex
  */
-const Regex = (pattern) => {
-  return {
+const Check = (...args) => _.mapValues(patterns, p => () => {
+  const valid = _.every(args, a => p.test(a));
+  if (!valid) throw new Meteor.Error('regex-police-alarm');
+});
 
-    /**
-     * Check if regex match string (Check.Cursor().match()).
-     * @memberof LernCheck.Regex()
-     * @example
-     * Check.Regex('email').match('lern@lern.com')
-     * @public
-     * @param {String} str - string to test
-     * @returns {Boolean}
-     */
-    match(str) {
-      return patterns.pattern.test(str);
-    },
-
-    /**
-     * Check if regex match string (Check.Cursor().match()).
-     * @memberof LernCheck.Regex()
-     * @example
-     * Check.Regex('email').check('lern@lern.com')
-     * @public
-     * @param {String} str - string to test
-     * @throws {Meteor.Error} regex-police-alarm
-     */
-    check(str) {
-      if (!patterns.pattern.test(str))
-        throw new Meteor.Error('regex-police-alarm');
-    },
-  };
-};
-
-export default Regex;
+export default Check;
