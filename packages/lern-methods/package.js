6eecd3330af05c1aac98bd04f5d6f9fa57d9b175
André Dornas <andredornas299@gmail.com>
2018-01-11 20:29:15 -0200

    define methods tests

--- packages/lern-methods/package.js
+++ packages/lern-methods/package.js
@@ -17,3 +17,23 @@ Package.onUse(function (api) {
     'server/admin/admin.js',
   ], 'server');
 });
+
+Package.onTest((api) => {
+
+  api.use([
+    'ecmascript',
+    'accounts-base@1.4.0',
+    'accounts-password@1.5.0',
+    'xolvio:cleaner@0.3.1',
+    'coffeescript@1.12.7_3',
+    'practicalmeteor:mocha@2.4.5_6',
+    'practicalmeteor:sinon@1.14.1_2',
+    'dispatch:phantomjs-tests@=0.0.5',
+    'dispatch:mocha-phantomjs',
+    'duckdodgerbrasl:lern-model',
+    'duckdodgerbrasl:lern-check',
+    'duckdodgerbrasl:lern-methods',
+  ]);
+
+  api.mainModule('tests.js');
+});
