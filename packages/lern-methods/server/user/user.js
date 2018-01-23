2c4cc43725b93f3083100460fab7558faf6e0cfa
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-01-23 11:52:10 -0200

    #79 fix method to return home route

---
+++ packages/lern-methods/server/user/user.js
@@ -0,0 +1,18 @@
+
+import _ from 'lodash';
+import { User } from 'meteor/duckdodgerbrasl:lern-model';
+import Helpers from '../../helpers.js';
+const [prefix, protect] = ['User'];
+
+Helpers.Methods({ prefix, protect }, {
+  GetInitialRoute(option) {
+    const userId = Meteor.userId();
+
+    const user = User.findOne(userId);
+
+    return _.get({
+      setup: user.getSetupRoute(),
+      home: user.getHomeRoute(),
+    }, option);
+  },
+});
