9f4398ff391452ef065549c0a3b7a0979510eec2
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-01-27 18:24:42 -0200

    #76 add chart to report

---
+++ client/views/Student/Report/Card.jsx
@@ -0,0 +1,131 @@
+// Libs
+import React from 'react';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import { Layout } from 'meteor/duckdodgerbrasl:lern-layouts';
+import i18n from 'meteor/universe:i18n';
+import { withStyles } from 'material-ui/styles';
+import classnames from 'classnames';
+import Typography from 'material-ui/Typography';
+import Card, { CardActions, CardContent, CardHeader } from 'material-ui/Card';
+import yellow from 'material-ui/colors/yellow';
+import Avatar from 'material-ui/Avatar';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import Icon from 'material-ui/Icon';
+import Collapse from 'material-ui/transitions/Collapse';
+import Divider from 'material-ui/Divider';
+
+import StudentReportChart from './Chart.jsx';
+
+const styles = theme => ({
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: yellow[700],
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class StudentReportCard extends React.Component {
+
+  // Lifecycle
+  constructor(props) {
+    super(props);
+    this.state = { expanded: false };
+  };
+
+  // Handlers
+
+  handleExpandClick = () => {
+    this.setState({ expanded: !this.state.expanded });
+  };
+
+  // Render
+
+  render() {
+    const { classes, report } = this.props;
+
+    console.log(report);
+
+    return (
+      <Card key={report.name}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              {_.head(report.name)}
+            </Avatar>
+          }
+          title={report.name}
+          subheader={`Total score: ${report.score || _.sum(_.map(report.childrens, 'score'))}`}
+        />
+
+        {
+          _.isEmpty(report.childrens)
+          ? null
+          : <CardActions className={classes.actions} disableActionSpacing>
+
+            <Button
+              onClick={() => this.handleDetailsClick(report)}
+              className={classes.button}
+            >
+              See More
+            </Button>
+
+            <Button
+              className={classes.button}
+              onClick={this.handleExpandClick}
+            >
+              See Details
+            </Button>
+
+            <IconButton
+              className={
+                classnames(classes.expand, { [classes.expandOpen]: this.state.expanded })
+              }
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label='Show more'
+            >
+              <Icon>expand_more</Icon>
+            </IconButton>
+
+          </CardActions>
+        }
+
+        {
+          _.isEmpty(report.childrens)
+          ? null
+          : <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Divider />
+              <StudentReportChart report={report} />
+            </CardContent>
+          </Collapse>
+        }
+
+      </Card>
+    );
+  };
+
+};
+
+StudentReportCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  report: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(StudentReportCard);;
--- client/views/Student/Report/View.jsx
+++ client/views/Student/Report/View.jsx
@@ -1,21 +1,60 @@
 // Libs
 import React from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { Layout } from 'meteor/duckdodgerbrasl:lern-layouts';
 import i18n from 'meteor/universe:i18n';
+import LinearProgress from 'material-ui/Progress'
+
+import StudentReportCard from './Card.jsx';
 
 class StudentReport extends React.Component {
+
+  // Lifecycle
+  constructor(props) {
+    super(props);
+    this.state = { report: null };
+  };
+
+  componentDidMount() {
+    const { user } = this.context;
+    this.setState({ report: _.get(user, 'report') });
+  };
 
   // Get Context
 
   contextTypes: {
     user: PropTypes.object,
-  }
+  };
+
+  // Handlers
+
+  handleDetailsClick = (report) => {
+    this.setState({ report });
+  };
+
+  // Render
 
   render() {
-    console.log(this.context);
+    const { classes } = this.props;
+
+    console.log(_.isEmpty(this.state.report)
+    ? `<LinearProgress color='primary' />`
+    : `_.isArray(this.state.report)`
+    ? `_.map(this.state.report, report => <StudentReportCard report={report} key={report.name}/>)`
+    : `<StudentReportCard report={this.state.report} />`);
+
     return <div>
       <Layout.Bar title={i18n.__('StudentReport.appBar')} />
+
+      {
+        _.isEmpty(this.state.report)
+        ? <LinearProgress color='primary' />
+        : _.isArray(this.state.report)
+        ? _.map(this.state.report, report => <StudentReportCard report={report} key={report.name}/>)
+        : <StudentReportCard report={this.state.report} />
+      }
+
     </div>;
   }
 };
