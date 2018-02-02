c8c8115e04a65bcf7c166d6ed10a264aa5b7458d
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-02-02 00:09:32 -0200

    #78 facebook email merge conflict

--- server/hooks/login.js
+++ server/hooks/login.js
@@ -15,14 +15,7 @@ Meteor.startup(() => {
           profilePic: `http://graph.facebook.com/${_.get(face, 'id')}/picture?type=square`,
           gender: _.get(face, 'gender'),
         };
-<<<<<<< HEAD
         user.emails = [];
-=======
-        if (_.get(face, 'email'))
-          user.emails = [{ address: _.get(face, 'email'), verified: true }];
-        else
-          user.emails = [];
->>>>>>> 804a0eae59e2746711211f21f4a85a6881443ae2
       } else if (_.get(user, 'services.google')) {
         const google = _.get(user, 'services.google');
         user.profile = {
