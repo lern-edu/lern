8a1303997ba58a8055a1bd1f46dca28b74091a3c
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-02-10 11:29:16 -0200

    #48 tag manegement finished

--- client/views/Admin/Tag/View.jsx
+++ client/views/Admin/Tag/View.jsx
@@ -12,6 +12,9 @@ import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import Input, { InputLabel } from 'material-ui/Input';
 import { FormControl, FormHelperText } from 'material-ui/Form';
+import Divider from 'material-ui/Divider/Divider';
+
+import AdminTagChildrens from './Childrens/Childrens.jsx';
 
 // Styles
 const styles = theme => ({
@@ -33,6 +36,7 @@ class AdminTag extends React.Component {
     const { tagId } = props;
     this.state = {
       title: !tagId ? 'Criar' : 'Editar',
+      crumbs: [{ label: 'Tags', path: 'AdminTags' }],
       collections: {
         tag: {
           handler: !_.isEmpty(tagId),
@@ -41,15 +45,37 @@ class AdminTag extends React.Component {
       doc: !tagId ? new Tag() : null,
       errors: {},
     };
-  }
+  };
+
+  shouldComponentUpdate(props, state) {
+    if (this.props.tagId === props.tagId)
+      return true;
+    else
+      this.getTags(props.tagId);
+    return false;
+  };
 
   componentWillMount() {
-    const { tagId } = this.props;
+    this.getTags(this.props.tagId);
+  };
+
+  getTags = (tagId) => {
 
     if (tagId)
-      Meteor.call('AdminTagsGet', { _id: tagId }, { limit: 1 },  (err, docs) => {
+      Meteor.call('AdminTagsGet', { _id: tagId }, { limit: 1 }, (err, docs) => {
         if (err) snack({ message: 'Erro ao encontrar tag' });
-        this.setState({ doc: _.head(docs), collections: { tag: { handler: false } } });
+        else {
+          const doc = _.head(docs);
+          this.setState({
+            doc,
+            title: doc.name,
+            crumbs: doc.parent
+              ? [{ label: doc.parent.name, path: FlowRouter.path('AdminTag', { tagId: doc.parent._id }) }]
+              : [{ label: 'Tags', path: 'AdminTags' }],
+            collections: { tag: { handler: false } },
+          });
+        };
+
       });
   };
 
@@ -66,28 +92,29 @@ class AdminTag extends React.Component {
 
   // Handlers
 
-  handleSubmit() {
+  handleSubmit = () => {
     const { doc } = this.state;
 
     doc.validate({ fields: ['name'] }, (err) => {
       if (err) snack({ message: err.reason });
       else
         Meteor.call('AdminTagSave', doc, (err, res) => {
-          if (err) snack({ message: 'Erro ao salvar tag' });
-          else FlowRouter.go('AdminTags');
+          if (err) {
+            snack({ message: 'Erro ao salvar tag' });
+            console.error(err);
+          }
+          else FlowRouter.go('AdminTag', { tagId: res._id });
         });
     });
   }
 
   render() {
-    const { title, collections, errors, doc } = this.state;
-    const { classes } = this.props;
-
-    console.log(doc);
+    const { title, collections, errors, doc, crumbs } = this.state;
+    const { classes, tagId } = this.props;
 
     return (
       <div>
-        <Layout.Bar title={title} crumbs={[{ label: 'Tags', path: 'AdminTags' }]}/>
+        <Layout.Bar title={title} crumbs={crumbs}/>
 
         {
           !_.every(collections, c => !c.handler)
@@ -126,10 +153,44 @@ class AdminTag extends React.Component {
 
                       <Grid item xs={12}>
                         <Paper className={classes.paper}>
-                          <doc.templates.Description form={this} doc={doc} />
+                          <doc.templates.DescriptionCreate form={this} doc={doc} />
                         </Paper>
                       </Grid>
+                      
+                      <Grid item xs={12}>
+                        {
+                          _.isEmpty(doc.description)
+                          ? undefined
+                          : <Paper className={classes.paper}>
+                            {
+                              _.map(doc.description, (description, index) =>
+                                [
+                                  <doc.templates.DescriptionShow
+                                    content={description}
+                                    key={`descriptionShow${index}`}
+                                  />,
+                                  <Divider key={`descriptionDivider${index}`}/>,
+                                ]
+                              )
+                            }
+                          </Paper>
+                        }
+                      </Grid>
+                      
 
+                      <Grid item xs={12}>
+
+                        {
+                          !tagId
+                          ? undefined
+                          : (
+                            <Paper className={classes.paper}>
+                              <AdminTagChildrens parent={doc} tagId={tagId} />
+                            </Paper>
+                          )
+                        }
+
+                      </Grid>
 
                       <Grid item xs={12}>
 
@@ -146,7 +207,7 @@ class AdminTag extends React.Component {
                           </Grid>
 
                           <Grid item>
-                            <Button onClick={this.handleSubmit.bind(this)} raised color='primary'>
+                            <Button onClick={this.handleSubmit} raised color='primary'>
                               Save
                             </Button>
                           </Grid>
