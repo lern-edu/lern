9968649dfacb9fd8101586efd2798afa2338a871
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-02-12 19:13:31 -0200

    #94 create test model

---
+++ packages/lern-model/collections/tests/schema.js
@@ -0,0 +1,85 @@
+import { Mongo } from 'meteor/mongo';
+import { Class } from 'meteor/jagi:astronomy';
+import _ from 'lodash';
+import Author from '../../behaviors/author.js';
+import Content from '../../schemas/content/schema.js';
+
+const Tests = new Mongo.Collection('tests');
+
+const TestTimeoutSchema = Class.create({
+  name: 'TestTimeout',
+  fields: {
+    type: {
+      type: String,
+    },
+    timeout: {
+      type: Number,
+      optional: true,
+    },
+    range: {
+      type: [Object],
+      optional: true,
+    },
+  },
+});
+
+const TestPageSchema = Class.create({
+  name: 'TestPage',
+  fields: {
+    content: {
+      type: [Object],
+      validators: [{ type: 'minLength', param: 1 }],
+    },
+  },
+});
+
+const TestAvalableAtSchema = Class.create({
+  name: 'TestAvalableAt',
+  fields: {
+    start: Date,
+    end: {
+      type: Date,
+      optional: true,
+    },
+  },
+});
+
+const Test = Class.create({
+  name: 'Test',
+  collection: Tests,
+  fields: {
+    name: String,
+    students: {
+      type: [String],
+      validators: [{ type: 'References' }],
+      optional: true,
+    },
+    tags: [Object],
+    pages: {
+      type: [TestPageSchema],
+      validators: [{ type: 'minLength', param: 1 }],
+    },
+    time: TestTimeSchema,
+    resolution: String,
+    help: {
+      type: [Content],
+      optional: true,
+    },
+    availableAt: {
+      type: TestAvalableAtSchema,
+      optional: true,
+    },
+  },
+  behaviors: {
+    timestamp: {
+      hasCreatedField: true,
+      createdFieldName: 'createdAt',
+      hasUpdatedField: true,
+      updatedFieldName: 'updatedAt',
+    },
+  },
+});
+
+Author(Test);
+
+export default Test;
