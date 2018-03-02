2dbf8fe10443a46a35b9a0a8e8c13389bbe643d4
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-03-02 08:53:17 -0300

    #106 fixing import

--- client/views/Public/Games/Sudoku/View.jsx
+++ client/views/Public/Games/Sudoku/View.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import { Typography, Button } from 'material-ui';
-import { Sudoku } from 'meteor/lern-games';
+import { Sudoku } from 'meteor/duckdodgerbrasl:lern-games';
 import { Layout } from 'meteor/duckdodgerbrasl:lern-layouts';
 
 const styles = theme => ({
