0b2fd48054ca68b9536195e031a2ad484815b7f4
André Dornas <andredornas299@gmail.com>
2018-03-08 15:06:45 -0300

    task show

--- packages/lern-model/schemas/content/template/Show/index.jsx
+++ packages/lern-model/schemas/content/template/Show/index.jsx
@@ -1,20 +1,21 @@
 // Libs
 import React from 'react';
 import _ from 'lodash';
-import { Paper, FlatButton } from 'material-ui';
+import { Paper, Typography } from 'material-ui';
 import { Editor, EditorState, convertFromRaw } from 'draft-js';
 
 // Views
 import PublicContentRichText from './../RichText.jsx';
 import PublicContentShowImage from './Image.jsx';
 import PublicContentShowVideo from './Video.jsx';
+import PublicContentShowTask from './Task.jsx';
 
 class ContentShow extends React.Component {
 
   // Render
 
   render() {
-    const { doc, doc: { text, link, image, type, video }, imgStyle } = this.props;
+    const { doc, doc: { text, link, image, type, video, task }, imgStyle } = this.props;
 
     return _.get({
       text:
@@ -33,6 +34,10 @@ class ContentShow extends React.Component {
       video:
         <PublicContentShowVideo
           video={video}
+        />,
+      task:
+        <PublicContentShowTask
+          task={task}
         />,
     }, type);
   };
