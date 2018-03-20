23bd2e18707da87caf2ecd6dd61d1edb8e83c986
André Dornas <andredornas299@gmail.com>
2018-03-19 22:04:25 -0300

    dialog for create page

--- client/views/Admin/Test/Pages.jsx
+++ client/views/Admin/Test/Pages.jsx
@@ -5,6 +5,7 @@ import { StaticCollections, Test, Content } from 'meteor/duckdodgerbrasl:lern-mo
 import Grid from 'material-ui/Grid';
 import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
 
 const content = new Content();
@@ -12,6 +13,7 @@ const ContentCreate = _.get(content, 'templates.ContentCreate');
 const ContentShow = _.get(content, 'templates.ContentShow');
 
 import AdminTestSelect from './Select.jsx';
+import AdminTestPage from './Page.jsx';
 
 // Styles
 const styles = theme => ({
@@ -22,12 +24,31 @@ const styles = theme => ({
 });
 
 class AdminTestPages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      index: -1,
+    };
+  }
+
+  createPage = () => {
+    const { doc } = this.props;
+    const { index } = this.state;
+    doc.pages.push(new Test.TestPageSchema());
+    this.setState({
+      open: true,
+      index: doc.pages.length - 1,
+    });
+  };
+
   // Render
   render() {
     const { doc, field, error, parent, classes } = this.props;
+    const { index } = this.state;
 
     if (doc.get('resolution') === 'content' && !doc.get('pages'))
-      doc.set('pages', [new Test.TestPageSchema()]);
+      doc.set('pages', []);
 
     return (
       <div>
@@ -40,41 +61,37 @@ class AdminTestPages extends React.Component {
 
                 <Grid item xs={12}>
 
-                  <Paper className={classes.paper}>
+                  <AdminTestPage
+                    open={this.state.open}
+                    handleClose={() => this.setState({ open: false })}
+                    doc={doc}
+                    parent={parent}
+                    field={`pages.${index}.description`}
+                    index={index}
+                  />
 
-                    <ContentCreate
-                      Schema={Content}
-                      doc={doc}
-                      field='pages.0.description'
-                      form={parent}
-                      contentTypes={StaticCollections.ContentTypes}
-                    />
-
-                  </Paper>
+                  <Button onClick={this.createPage}>
+                    Criar Página
+                  </Button>
 
                 </Grid>
 
                 <Grid item xs={12}>
-                  {
-                    _.isEmpty(_.get(doc, 'pages.0.description'))
-                      ? undefined
-                      : <Paper className={classes.paper}>
-                        {
-                          _.map(_.get(doc, 'pages.0.description'), (description, index) =>
-                            [
-                              <ContentShow
-                                doc={description}
-                                form={parent}
-                                index={index}
-                                canRemove={true}
-                                key={`pages.0.descriptionShow${index}`}
-                              />,
-                              <Divider key={`pages.0.descriptionDivider${index}`} />,
-                            ]
-                          )
-                        }
-                      </Paper>
-                  }
+                  <Grid container spacing={8}>
+                    {_.map(_.get(doc, 'pages'), (page, idx) =>
+                      <Grid
+                        item
+                        xs={12} sm={6} md={4} lg={3}
+                        key={`pages.${idx}`}
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => this.setState({ index: idx, open: true })}
+                      >
+                        <Paper className={classes.paper}>
+                          <h1>{'Page' + idx}</h1>
+                        </Paper>
+                      </Grid>
+                    )}
+                  </Grid>
                 </Grid>
 
             </Grid>
