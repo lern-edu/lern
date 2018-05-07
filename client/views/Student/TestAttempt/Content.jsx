a2c8c9fafe761780eecbd24a0e285a48057e0865
Douglas Eleuterio <douglaseleuterio@gmail.com>
2018-05-07 08:46:16 -0300

    #157 Content open everything

--- client/views/Student/TestAttempt/Content.jsx
+++ client/views/Student/TestAttempt/Content.jsx
@@ -2,21 +2,26 @@
 import React from 'react';
 import _ from 'lodash';
 import log from 'loglevel';
+import { Content } from 'meteor/duckdodgerbrasl:lern-model';
 import PropTypes from 'prop-types';
+// Material Components
 import List, { ListItem, ListItemText } from 'material-ui/List';
 import { withStyles } from 'material-ui/styles';
 import Avatar from 'material-ui/Avatar';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
-import { Content } from 'meteor/duckdodgerbrasl:lern-model';
 import MobileStepper from 'material-ui/MobileStepper';
 import Button from 'material-ui/Button';
 import KeyboardArrowLeft from 'material-ui-icons/KeyboardArrowLeft';
 import KeyboardArrowRight from 'material-ui-icons/KeyboardArrowRight';
 
+// Views
 const content = new Content();
 const ContentShow = _.get(content, 'templates.ContentShow');
+import StudentTestAttemptSudoku from './Sudoku.jsx';
+import StudentTestAttemptQuestion from './Question.jsx';
 
+// Styles
 const styles = theme => ({
   paper: {
     padding: theme.spacing.unit * 2,
@@ -32,59 +37,10 @@ const styles = theme => ({
 
 class StudentTestAttemptContent extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeStep: 0,
-    };
-  }
-
-  // Lifecycle
-  // componentDidMount() {
-  //   log.info('StudentTestAttemptContent.componentDidMount =>', this.props);
-  //   const { parent } = this.props;
-
-  //   this.verifyScroll = (event) => {
-  //     if ($(window).scrollTop() + $(window).height() === this.getDocHeight())
-  //       parent.setState({ bottom: 'continue' });
-  //     else parent.setState({ bottom: null });
-  //   };
-
-  //   this.verifyScroll();
-  //   window.addEventListener('scroll', this.verifyScroll);
-  // };
-
-  // componentWillUnmount() {
-  //   log.info('StudentTestAttemptContent.componentWillUnmount');
-  //   window.removeEventListener('scroll', this.verifyScroll);
-  // }
-
-  // // Util
-  // getDocHeight = () => {
-  //   const D = document;
-  //   return Math.max(
-  //     D.body.scrollHeight, D.documentElement.scrollHeight,
-  //     D.body.offsetHeight, D.documentElement.offsetHeight,
-  //     D.body.clientHeight, D.documentElement.clientHeight
-  //   );
-  // };
-
-  handleNext = () => {
-    this.setState({
-      activeStep: this.state.activeStep + 1,
-    });
-  };
-
-  handleBack = () => {
-    this.setState({
-      activeStep: this.state.activeStep - 1,
-    });
-  };
-
   // Render
   render() {
     log.info('StudentTestAttemptContent.render =>', this.props);
-    const { pages, page, classes, theme } = this.props;
+    const { pages, page, classes, attempt } = this.props;
 
     return (
       <div style={{ width: '100%' }}>
@@ -93,6 +49,7 @@ class StudentTestAttemptContent extends React.Component {
             <Grid item xs={12} key={`descriptionShow${index}`}>
 
               {
+                // Render image
                 description.type === 'image'
                 ? (
                   <Grid container justify='center' >
@@ -105,8 +62,24 @@ class StudentTestAttemptContent extends React.Component {
                     </Grid>
                   </Grid>
                 )
-                : <ContentShow doc={ description } canRemove={false} />
-            }
+
+                // Render text
+                : description.type === 'text'
+                ? <ContentShow doc={description} canRemove={false} />
+
+                // Render question
+                : description.type === 'question'
+                ? <StudentTestAttemptQuestion
+                  doc={description.question}
+                  page={page}
+                  attempt={attempt}
+                />
+
+                // Render sudoku
+                : description.type === 'sudoku'
+                ? <StudentTestAttemptSudoku />
+                : undefined
+              }
 
             </Grid>
           )
@@ -120,7 +93,8 @@ class StudentTestAttemptContent extends React.Component {
 StudentTestAttemptContent.propTypes = {
   pages: PropTypes.array.isRequired,
   page: PropTypes.number.isRequired,
+  attempt: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(StudentTestAttemptContent);
+export default withStyles(styles)(StudentTestAttemptContent);
